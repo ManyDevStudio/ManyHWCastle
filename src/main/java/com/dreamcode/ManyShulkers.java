6cbe9ec414ee0366a9e1d62b0c6527fd77f0fbf0
Kirill Bulygin <68967081+kbkirill@users.noreply.github.com>
2025-03-14 20:59:42 +0200

    Integrated lombok v2

--- src/main/java/com/dreamcode/ManyShulkers.java
+++ src/main/java/com/dreamcode/ManyShulkers.java
@@ -1,21 +1,22 @@
 package com.dreamcode;
 
 import co.aikar.commands.PaperCommandManager;
+import lombok.Getter;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 import com.dreamcode.command.ManyShulkersCommand;
 import com.dreamcode.config.Config;
 import com.dreamcode.listener.BreakListener;
 
+@Getter
 public class ManyShulkers extends JavaPlugin {
-    private Config config;
+    private Config configuration;
     private LootManager lootManager;
     private PaperCommandManager paperCommandManager;
     @Override
     public void onEnable() {
-        config = new Config(this);
-        config.load();
+        configuration = new Config(this);
+        configuration.load();
         lootManager = new LootManager(this);
         paperCommandManager = new PaperCommandManager(this);
         paperCommandManager.registerCommand(new ManyShulkersCommand(this));
@@ -33,16 +34,4 @@ public class ManyShulkers extends JavaPlugin {
         Bukkit.getServer().getPluginManager().registerEvents(new BreakListener(this), this);
     }
 
-    @NotNull
-    public Config getConfiguration() {
-        return config;
-    }
-
-    public LootManager getLootManager() {
-        return lootManager;
-    }
-
-    public PaperCommandManager getPaperCommandManager() {
-        return paperCommandManager;
-    }
 }
